4d8f0ce2bbba13931bc881f2eef68e12c952c85e
Aaron1357 <kimjonghwi1540@gmail.com>
2023-11-03 15:19:53 +0900

    test++

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Header from "@/components/Header";
+export default function App({Component , pageProps}) {
+  return (
+    <div>
+      <Header/>
+      <Component {...pageProps}/>
+      <Head>
+        <title>유어썸뷰티 | About Us</title>
+      </Head>
+
+    </div>
+  );
+}

1d69b4069049758570927c05dac4a398f80e443d
구관원 <rksjsl@naver.com>
2023-11-09 21:39:14 +0900

    푸시~

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { Link } from "react-scroll";
 import styled from "styled-components";
 import React, { useState, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
 
 
-const Test = styled.div`
-
-
+const HeadisDesktop = styled.div`
 @font-face {
   font-family: "Elice";
   font-weight: 200;
@@ -20,15 +19,17 @@ body {
   margin: 0;
   padding: 0;
 }
+
 .main-head {
   display: flex;
   background-color: lavenderblush;
   width: 100%;
-  height: 11%;
+  height: 110px;
   position: fixed; /* 헤더를 고정시킴 */
   top: 0; /* 화면 상단에 고정 */
   z-index: 1000; /* 다른 콘텐츠 위에 표시 */
 }
+
 .Logo-holder {
   padding-left: 500px;
   padding-right: 300px;
@@ -52,6 +53,132 @@ body {
     transition: color 0.5s;
   }
 }
+`;
+
+
+const HeadisTablet = styled.div`
+@font-face {
+  font-family: "Elice";
+  font-weight: 200;
+  src: url("/fonts/EliceDXNeolli-Medium.ttf") format("truetype");
+}
+@font-face {
+  font-family: "Elice-light";
+  font-weight: 200;
+  src: url("/fonts/EliceDXNeolli-Light.ttf") format("truetype");
+}
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.main-head {
+  display: flex;
+  background-color: red;
+  width: 100%;
+  height: 110px;
+  position: fixed; /* 헤더를 고정시킴 */
+  top: 0; /* 화면 상단에 고정 */
+  z-index: 1000; /* 다른 콘텐츠 위에 표시 */
+}
+
+.Logo-holder {
+  padding-left: 500px;
+  padding-right: 300px;
+}
+.Logo {
+  width: 190px;
+  height: 110px;
+}
+.menu-holder {
+  display: flex;
+  align-items: center;
+  padding: 30px;
+}
+.menu {
+  font-size: 18px;
+  color: black;
+  font-family: Elice-light;
+  &:hover {
+    color: red;
+    opacity: 0.5;
+    transition: color 0.5s;
+  }
+}
+`;
+
+const HeadisMobile = styled.div`
+@font-face {
+  font-family: "Elice";
+  font-weight: 200;
+  src: url("/fonts/EliceDXNeolli-Medium.ttf") format("truetype");
+}
+@font-face {
+  font-family: "Elice-light";
+  font-weight: 200;
+  src: url("/fonts/EliceDXNeolli-Light.ttf") format("truetype");
+}
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.main-head {
+  display: flex;
+  background-color: red;
+  width: 100%;
+  height: 110px;
+  position: fixed; /* 헤더를 고정시킴 */
+  top: 0; /* 화면 상단에 고정 */
+  z-index: 1000; /* 다른 콘텐츠 위에 표시 */
+}
+
+.Logo-holder {
+  padding-left: 500px;
+  padding-right: 300px;
+}
+.Logo {
+  width: 190px;
+  height: 110px;
+}
+.menu-holder {
+  display: flex;
+  align-items: center;
+  padding: 30px;
+}
+.menu {
+  font-size: 18px;
+  color: black;
+  font-family: Elice-light;
+  &:hover {
+    color: red;
+    opacity: 0.5;
+    transition: color 0.5s;
+  }
+}
+`;
+
+
+const Test = styled.div`
+
+
+@font-face {
+  font-family: "Elice";
+  font-weight: 200;
+  src: url("/fonts/EliceDXNeolli-Medium.ttf") format("truetype");
+}
+@font-face {
+  font-family: "Elice-light";
+  font-weight: 200;
+  src: url("/fonts/EliceDXNeolli-Light.ttf") format("truetype");
+}
+body {
+  margin: 0;
+  padding: 0;
+}
+
+
+
 
 .mainPhoto {
   max-height: 89%;
@@ -272,12 +399,21 @@ transform: translateX(170%);
     transition: transform 0.5s ease;
   }
 
- 
 
 
 `;
 
 function App() {
+
+  const isDesktop = useMediaQuery({
+    query: "(min-width:1024px)",
+  });
+  const isTablet = useMediaQuery({
+    query: "(min-width:768px) and (max-width:1023px)",
+  });
+  const isMobile = useMediaQuery({
+    query: "(max-width:767px)",
+  });
 
   const images = ["/place1.png", "/place2.png"]; // 이미지 파일 경로 배열
 
@@ -397,8 +533,9 @@ function App() {
 
 
   return (
-    <Test>
-    <div className="App">
+    <>
+      {isDesktop && (<HeadisDesktop>
+        <div className="App">
       <div className="main-head">
         <div className="Logo-holder">
           <img src="/Logo.png" className="Logo" alt="img" /> {/*로고 이미지 */}
@@ -453,7 +590,123 @@ function App() {
         </div>
       </div>
     </div>
-    
+    </HeadisDesktop>)}
+    {isTablet && (<HeadisTablet>
+        <div className="App">
+      <div className="main-head">
+        <div className="Logo-holder">
+          <img src="/Logo.png" className="Logo" alt="img" /> {/*로고 이미지 */}
+        </div>
+        <div className="menu-holder">
+          <Link
+            to="us-con" // 섹션의 ID를 여기에 입력
+            spy={true}
+            smooth={true}
+            offset={-20} // 스크롤 오프셋 조절 (헤더 높이에 맞게 조절)
+            duration={500}
+          >
+            <div className="menu"> About us </div>
+          </Link>
+          {/* <div className="menu"> About us </div> 메뉴 1 */}
+        </div>
+        <div className="menu-holder">
+          <Link
+            to="inner-sec" // 섹션의 ID를 여기에 입력
+            spy={true}
+            smooth={true}
+            offset={-20} // 스크롤 오프셋 조절 (헤더 높이에 맞게 조절)
+            duration={500}
+          >
+            <div className="menu"> Gallary </div>
+          </Link>
+          {/* <div className="menu"> Gallary </div> 메뉴 2 */}
+        </div>
+        <div className="menu-holder">
+          <Link
+            to="QnA-con" // 섹션의 ID를 여기에 입력
+            spy={true}
+            smooth={true}
+            offset={-20} // 스크롤 오프셋 조절 (헤더 높이에 맞게 조절)
+            duration={500}
+          >
+            <div className="menu"> Reservation </div>
+          </Link>
+          {/* <div className="menu"> Reservation  </div> 메뉴 3 */}
+        </div>
+        <div className="menu-holder">
+          <Link
+            to="map-sec" // 섹션의 ID를 여기에 입력
+            spy={true}
+            smooth={true}
+            offset={-90} // 스크롤 오프셋 조절 (헤더 높이에 맞게 조절)
+            duration={500}
+          >
+            <div className="menu"> Location </div>
+          </Link>
+          {/* <div className="menu"> Location </div> 메뉴 4 */}
+        </div>
+      </div>
+    </div>
+    </HeadisTablet>)}
+    {isMobile && (<HeadisMobile>
+        <div className="App">
+      <div className="main-head">
+        <div className="Logo-holder">
+          <img src="/Logo.png" className="Logo" alt="img" /> {/*로고 이미지 */}
+        </div>
+        <div className="menu-holder">
+          <Link
+            to="us-con" // 섹션의 ID를 여기에 입력
+            spy={true}
+            smooth={true}
+            offset={-20} // 스크롤 오프셋 조절 (헤더 높이에 맞게 조절)
+            duration={500}
+          >
+            <div className="menu"> About us </div>
+          </Link>
+          {/* <div className="menu"> About us </div> 메뉴 1 */}
+        </div>
+        <div className="menu-holder">
+          <Link
+            to="inner-sec" // 섹션의 ID를 여기에 입력
+            spy={true}
+            smooth={true}
+            offset={-20} // 스크롤 오프셋 조절 (헤더 높이에 맞게 조절)
+            duration={500}
+          >
+            <div className="menu"> Gallary </div>
+          </Link>
+          {/* <div className="menu"> Gallary </div> 메뉴 2 */}
+        </div>
+        <div className="menu-holder">
+          <Link
+            to="QnA-con" // 섹션의 ID를 여기에 입력
+            spy={true}
+            smooth={true}
+            offset={-20} // 스크롤 오프셋 조절 (헤더 높이에 맞게 조절)
+            duration={500}
+          >
+            <div className="menu"> Reservation </div>
+          </Link>
+          {/* <div className="menu"> Reservation  </div> 메뉴 3 */}
+        </div>
+        <div className="menu-holder">
+          <Link
+            to="map-sec" // 섹션의 ID를 여기에 입력
+            spy={true}
+            smooth={true}
+            offset={-90} // 스크롤 오프셋 조절 (헤더 높이에 맞게 조절)
+            duration={500}
+          >
+            <div className="menu"> Location </div>
+          </Link>
+          {/* <div className="menu"> Location </div> 메뉴 4 */}
+        </div>
+      </div>
+    </div>
+    </HeadisMobile>)}
+            
+    <Test>
       <div>
         <div>
           <img className="mainPhoto"  alt="img" src={images[currentImageIndex]} />
@@ -507,6 +760,7 @@ function App() {
 
           <div className="QnA-con">
             <h1 className="QnA-index">| Q & A |</h1>
+            
             <div className="QnA-welcome1">
               {" "}
               궁금하신 모든 사항은 편하게 문의주세요!{" "}
@@ -689,10 +943,8 @@ function App() {
             </tbody>
         </table>
       </div>
-
-
-
     </Test>
+    </>
   );
 }
 
